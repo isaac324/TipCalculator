1615c7328438949bfc8b84ab7c224051a5c52b33
Isaac <isaavila@csumb.edu>
2019-09-02 23:46:08 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Isaac Avila on 9/2/19.
+//  Copyright © 2019 Isaac Avila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
